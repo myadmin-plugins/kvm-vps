5dc74030b37d17055de699e51ffc6fdf723cc267
Joe Huss <detain@interserver.net>
2017-06-08 00:59:24 -0400

    plugin definition updates

--- myadmin.php
+++ myadmin.php
@@ -8,17 +8,17 @@ return [
 	'name' => 'Kvm Vps',
 	'description' => 'Allows selling of Kvm Server and VPS License Types.  More info at https://www.netenberg.com/kvm.php',
 	'help' => 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a kvm license. Allow 10 minutes for activation.',
-	'module' => 'licenses',
+	'module' => 'vps',
 	'author' => 'detain@interserver.net',
 	'home' => 'https://github.com/detain/myadmin-kvm-vps',
 	'repo' => 'https://github.com/detain/myadmin-kvm-vps',
 	'version' => '1.0.0',
-	'type' => 'licenses',
+	'type' => 'service',
 	'hooks' => [
 		/*'function.requirements' => ['Detain\MyAdminKvm\Plugin', 'Requirements'],
-		'licenses.settings' => ['Detain\MyAdminKvm\Plugin', 'Settings'],
-		'licenses.activate' => ['Detain\MyAdminKvm\Plugin', 'Activate'],
-		'licenses.change_ip' => ['Detain\MyAdminKvm\Plugin', 'ChangeIp'],
+		'vps.settings' => ['Detain\MyAdminKvm\Plugin', 'Settings'],
+		'vps.activate' => ['Detain\MyAdminKvm\Plugin', 'Activate'],
+		'vps.change_ip' => ['Detain\MyAdminKvm\Plugin', 'ChangeIp'],
 		'ui.menu' => ['Detain\MyAdminKvm\Plugin', 'Menu'] */
 	],
 ];
